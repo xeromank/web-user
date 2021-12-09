1262ad03f91bfaabbefcd8c1d24d32d6808e6de0
xeroman_kyn <xeroman.kyn@aceenter.com>
2021-12-09 13:51:54 +0900

    add axios

--- src/main.js
+++ src/main.js
@@ -3,5 +3,8 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App).use(store).use(router).mount('#app');
+
+app.config.globalProperties.axios = axios;
